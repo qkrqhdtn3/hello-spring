d6f727029b7ddd720831dd0a2f97e1e6c4aa01d0
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2022-12-08 17:22:10 +0900

    feat: Implement repository using jdbc, jdbcTemplate

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,7 +14,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-//    @Autowired
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
