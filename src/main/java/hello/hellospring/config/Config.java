e28254daa525d31e4f70baf3676093cb9054fa1f
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2022-12-08 18:01:36 +0900

    feat: Implement repository using Jpa

---
+++ src/main/java/hello/hellospring/config/Config.java
@@ -0,0 +1,13 @@
+package hello.hellospring.config;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+//    @Bean
+//    public EntityManager entityManager(){
+//        return new EntityManager();
+//    }
+}
